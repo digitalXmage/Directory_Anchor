53b2d3eb39794dbff623e2eee099e5824ce09bcf
Sam Dhillon <sam.dhillon1997@gmail.com>
2020-03-29 17:49:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc src/anchor.c config/config.h -o anchor
+gcc -W src/anchor.c config/config.h -o anchor
 sudo mv anchor /usr/bin/
 rm ~/.anchor;touch ~/.anchor
 cp ~/.bashrc ~/.bashrc.backup
